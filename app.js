b1efd7ecd9e3ae47a6a3ae1282a78edf6e161b7b
wharinas42@gmail.com <wharinas42@gmail.com>
2022-08-12 10:38:55 +0700

    update code

--- app.js
+++ app.js
@@ -13,19 +13,17 @@ const view = (res, path) => {
 app.get('/', (req, res) => {
 	return (view(res, '/views/index.html'));
 });
-app.get('/api/:date?', (req, res) => 
+app.get('/api/:date_input?', (req, res) => 
 {
-	const week_list      = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-	const month_list     = [0, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-	const re_format_date = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-	const value      = re_format_date.test(req.params.date) ? req.params.date : parseInt(req.params.date) ;
-	const date_value = new Date(value);
-	const utc_day   = week_list[date_value.getDay()];
-	const utc_date  = [date_value.getDate(), month_list[date_value.getMonth()], date_value.getYear()].join(' ');
-	const utc_time  = [('0' + date_value.getHours()).substr(-2), ('0' + date_value.getMinutes()).substr(-2), ('0' + date_value.getSeconds()).substr(-2)].join(':');
-	const utc_value = utc_day + ", " + utc_date + " " + utc_time + " GT";
+	const date_value  = (/\d{5,}/).test(req.params.date_input)?parseInt(req.params.date_input):req.params.date_input;
+	const date_object = new Date(date_value);
+	let result        = [];
 
-	res.json({ unix:Math.floor(date_value.getTime() / 1000), utc:utc_value });
+	if (date_object == "Invalid Date")
+		result = { error:date_object };
+	else
+		result = { unix:date_object.valueOf(), utc:date_object.toUTCString() };
+	res.json(result);
 });
 
 
